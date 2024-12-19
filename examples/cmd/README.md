99e568a7776eb18d93e87f31f184b9c12a9bd399
KrisPett <70701718+KrisPett@users.noreply.github.com>
2024-12-19 15:34:35 +0100

    commit

---
+++ examples/cmd/README.md
@@ -0,0 +1,2 @@
+- python examples/cmd/run.py say this is a test
+- python examples/cmd/jj.py
--- examples/cmd/jj.py
+++ examples/cmd/jj.py
@@ -1,21 +1,44 @@
-import ChatTTS
+# import ChatTTS
+# import torch
+# import torchaudio
+#
+# chat = ChatTTS.Chat()
+# chat.load(compile=False) # Set to True for better performance
+#
+# texts = ["Use print(torchaudio.list_audio_backends()) to see which backends are available in your current environment. If it returns an empty list, no backend is detected.", "For other systems or if you're not using Conda, download FFmpeg from its official site and add it to your system PATH."]
+#
+# wavs = chat.infer(texts)
+#
+# for i in range(len(wavs)):
+#     """
+#     In some versions of torchaudio, the first line works but in other versions, so does the second line.
+#     """
+#     try:
+#         torchaudio.save(f"basic_output{i}.wav", torch.from_numpy(wavs[i]).unsqueeze(0), 24000)
+#     except:
+#         torchaudio.save(f"basic_output{i}.wav", torch.from_numpy(wavs[i]), 24000)
+
+
+
+
 import torch
 import torchaudio
+torch._dynamo.config.cache_size_limit = 64
+torch._dynamo.config.suppress_errors = True
+torch.set_float32_matmul_precision('high')
+
+import ChatTTS
 
 chat = ChatTTS.Chat()
-chat.load(compile=False) # Set to True for better performance 
+chat.load(compile=False)
 
-texts = ["Use print(torchaudio.list_audio_backends()) to see which backends are available in your current environment. If it returns an empty list, no backend is detected.", "For other systems or if you're not using Conda, download FFmpeg from its official site and add it to your system PATH."]
+texts = [
+    "Yes, this is a test of the emergency broadcast system.",
+    ]
 
 wavs = chat.infer(texts)
 
-for i in range(len(wavs)):
-    """
-    In some versions of torchaudio, the first line works but in other versions, so does the second line.
-    """
-    try:
-        torchaudio.save(f"basic_output{i}.wav", torch.from_numpy(wavs[i]).unsqueeze(0), 24000)
-    except:
-        torchaudio.save(f"basic_output{i}.wav", torch.from_numpy(wavs[i]), 24000)
-
-
+try:
+    torchaudio.save(f"basic_output{0}.wav", torch.from_numpy(wavs[0]).unsqueeze(0), 24000)
+except:
+    torchaudio.save(f"basic_output{0}.wav", torch.from_numpy(wavs[0]), 24000)
